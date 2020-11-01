2e96123526e6a06fe1150cda8a53a3e8c7ce9def
Rithesh Rai <ritheshrai5@gmail.com>
2020-11-01 18:13:07 +0530

    first

--- DinoGame.py
+++ DinoGame.py
@@ -1,10 +1,11 @@
 import pyautogui
-from PIL import Image,ImageGrab
-def takescreenshot():
-    image=ImageGrab.grab()
+from PIL import Image, ImageGrab
+import time
+
+def takess():
+    image = ImageGrab.grab()
     image.show()
 
 
-
-if __name__="__main__":
-    takescreenshot()
+if __name__ == "__main__":
+    takess()
